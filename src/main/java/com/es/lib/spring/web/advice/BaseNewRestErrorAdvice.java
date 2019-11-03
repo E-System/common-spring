a523d8981470f352bbd40a03549bf508360aff4d
diman <zuzoev.d@ext-system.com>
2019-11-03 15:10:40 +0700

    Add new rest controller and advice for clear response

---
+++ src/main/java/com/es/lib/spring/web/advice/BaseNewRestErrorAdvice.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2016 E-System LLC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.es.lib.spring.web.advice;
+
+import com.es.lib.dto.DTOResult;
+import com.es.lib.dto.DTOValidationResult;
+import com.es.lib.dto.validation.DTOValidationField;
+import com.es.lib.dto.validation.DTOValidationStatus;
+import com.es.lib.spring.exception.ServiceException;
+import com.es.lib.spring.exception.ServiceValidationException;
+import com.es.lib.spring.service.EnvironmentProfileService;
+import com.es.lib.spring.service.controller.MessageService;
+import com.es.lib.spring.util.ErrorCodes;
+import com.es.lib.spring.web.common.BaseNewRestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * @author Zuzoev Dmitry - zuzoev.d@ext-system.com
+ * @since 03.11.19
+ */
+@ControllerAdvice(assignableTypes = BaseNewRestController.class)
+public class BaseNewRestErrorAdvice {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BaseNewRestErrorAdvice.class);
+
+    private MessageService messageService;
+    private EnvironmentProfileService environmentProfileService;
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    private DTOValidationResult validation(ServiceValidationException e) {
+        LOG.error(e.getCode(), e);
+        return new DTOValidationResult(
+            messageService.get(e.getCode(), e.getArgs()),
+            e.getErrorCode(),
+            e.getStatus().getFields()
+        );
+    }
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+    public DTOValidationResult violations(ConstraintViolationException e) {
+        return new DTOValidationResult(
+            "Invalid parameters",
+            ErrorCodes.VALIDATION,
+            create(e).getFields()
+        );
+    }
+
+    private DTOValidationStatus create(ConstraintViolationException e) {
+        Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+        Collection<DTOValidationField> fields = new ArrayList<>(violations.size());
+
+        for (ConstraintViolation<?> violation : violations) {
+            fields.add(new DTOValidationField(violation.getPropertyPath().toString(), violation.getMessage()));
+        }
+        return new DTOValidationStatus(DTOValidationStatus.Type.Error, fields);
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public DTOResult serviceException(ServiceException e) {
+        String message = messageService.getWithLocalizationCheck(e);
+        return new DTOResult(
+            message,
+            e.getErrorCode()
+        );
+    }
+
+    @ExceptionHandler(value = {Throwable.class})
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public DTOResult throwable(Throwable e) {
+        LOG.error("Service exception: " + e.getMessage(), e);
+        String message;
+        if (environmentProfileService.isDevelop()) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            message = e.getMessage();
+            //+ "\n" + sw.toString();
+        } else {
+            message = "Произошла ошибка. Обратитесь в техническую поддержку";
+        }
+        return new DTOResult(
+            message,
+            ErrorCodes.THROWABLE
+        );
+    }
+
+    @Autowired
+    public void setMessageService(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @Autowired
+    public void setEnvironmentProfileService(EnvironmentProfileService environmentProfileService) {
+        this.environmentProfileService = environmentProfileService;
+    }
+}
---
+++ src/main/java/com/es/lib/spring/web/common/BaseNewRestController.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2016 E-System LLC
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.es.lib.spring.web.common;
+
+import com.es.lib.dto.DTOResponse;
+import com.es.lib.dto.ResponseBuilder;
+import com.es.lib.dto.validation.DTOValidationField;
+import com.es.lib.dto.validation.DTOValidationStatus;
+import com.es.lib.spring.config.Constant;
+import com.es.lib.spring.exception.ServiceValidationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.Validator;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+/**
+ * @author Zuzoev Dmitry - zuzoev.d@ext-system.com
+ * @since 03.11.19
+ */
+public class BaseNewRestController extends BaseController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BaseNewRestController.class);
+
+    protected DTOResponse ok() {
+        return new ResponseBuilder<>(null).build();
+    }
+
+    protected <T> DTOResponse<T> ok(T data) {
+        return new ResponseBuilder<T>(null).data(data).build();
+    }
+
+    protected void checkError(BindingResult bindingResult, Object request, Validator... validators) {
+        checkError(Constant.System.VALIDATION_ERROR_CODE, bindingResult, request, validators);
+    }
+
+    protected void checkError(String messageCode, BindingResult bindingResult, Object request, Validator... validators) {
+        if (!bindingResult.hasErrors()) {
+            for (Validator validator : validators) {
+                validator.validate(request, bindingResult);
+            }
+        }
+        checkError(messageCode, bindingResult);
+    }
+
+    protected void checkError(Errors bindingResult) {
+        checkError(Constant.System.VALIDATION_ERROR_CODE, bindingResult);
+    }
+
+    protected void checkError(String messageCode, Errors bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Collection<DTOValidationField> fields = new LinkedList<>();
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                String message = messageService.get(fieldError);
+                LOG.error("BaseRestController::checkError - {}, {}", message, fieldError);
+                fields.add(new DTOValidationField(fieldError.getField(), message));
+            }
+            throw new ServiceValidationException(new DTOValidationStatus(DTOValidationStatus.Type.Error, fields), messageCode);
+        }
+    }
+}
