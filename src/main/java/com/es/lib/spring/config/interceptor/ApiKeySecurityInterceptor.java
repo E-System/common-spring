fae835afb0f08a5d4b460aa8965490b1fa30cb03
diman <zuzoev.d@ext-system.com>
2021-03-19 14:15:56 +0700

    Add default register

--- src/main/java/com/es/lib/spring/config/interceptor/ApiKeySecurityInterceptor.java
+++ src/main/java/com/es/lib/spring/config/interceptor/ApiKeySecurityInterceptor.java
@@ -11,6 +11,8 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -30,6 +32,18 @@ public class ApiKeySecurityInterceptor implements HandlerInterceptor {
             return false;
         }
         return true;
+    }
+
+    public static void register(InterceptorRegistry registry, ApiKeySecurityInterceptor interceptor) {
+        register(registry, interceptor, Collections.singletonList("/*"), Arrays.asList(
+            "/",
+            "/version",
+            "/docs",
+            "/swagger-ui.html**",
+            "/swagger-resources/**",
+            "/webjars/**",
+            "/v2/api-docs"
+        ));
     }
 
     public static void register(InterceptorRegistry registry, ApiKeySecurityInterceptor interceptor, List<String> includePatterns, List<String> excludePattern) {
