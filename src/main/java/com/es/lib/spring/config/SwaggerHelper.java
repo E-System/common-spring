c6fc0f0ff85d95fae8606ff5daa72e52de62079a
diman <zuzoev.d@ext-system.com>
2022-03-05 16:06:34 +0700

    Add SwaggerHelper component

---
+++ src/main/java/com/es/lib/spring/config/SwaggerHelper.java
@@ -0,0 +1,30 @@
+package com.es.lib.spring.config;
+
+import com.es.lib.spring.service.BuildInfoService;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletContext;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class SwaggerHelper {
+
+    private final ServletContext servletContext;
+    private final BuildInfoService buildInfoService;
+
+    public OpenAPI openApi(String title, String contactName, String contactEmail) {
+        return new OpenAPI()
+            .servers(Collections.singletonList(new Server().url(servletContext.getContextPath()).description("Default server url")))
+            .info(new Info()
+                .title(title)
+                .version(buildInfoService.getInfo().getVersion())
+                .contact(new Contact().name(contactName).email(contactEmail)));
+    }
+}
