a54d05f51e9e40ee34a2f4facb37f47fdef9101b
diman <zuzoev.d@ext-system.com>
2025-08-15 11:13:54 +0700

    Extract allowed headers

--- src/main/java/com/es/lib/spring/config/AllowAllCorsFilter.java
+++ src/main/java/com/es/lib/spring/config/AllowAllCorsFilter.java
@@ -19,6 +19,8 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 
 public abstract class AllowAllCorsFilter implements Filter {
 
@@ -29,13 +31,22 @@ public abstract class AllowAllCorsFilter implements Filter {
     private static final String MAX_AGE_NAME = "Access-Control-Max-Age";
     private static final String HEADERS_NAME = "Access-Control-Allow-Headers";
 
+    protected static final Collection<String> DEFAULT_ALLOWED_HEADERS = Arrays.asList(
+        "x-requested-with",
+        "authorization",
+        "Content-Type",
+        "Authorization",
+        "credential",
+        "X-XSRF-TOKEN"
+    );
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         config = filterConfig;
     }
 
     @Override
-    public void destroy() { }
+    public void destroy() {}
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
@@ -53,6 +64,6 @@ public abstract class AllowAllCorsFilter implements Filter {
         response.setHeader(ORIGIN_NAME, "*");
         response.setHeader(METHODS_NAME, "OPTIONS, GET, POST, PUT, DELETE");
         response.setHeader(MAX_AGE_NAME, "3600");
-        response.setHeader(HEADERS_NAME, "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+        response.setHeader(HEADERS_NAME, String.join(", ", DEFAULT_ALLOWED_HEADERS));
     }
 }
