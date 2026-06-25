67105950e0617a2433d9173c46ef125288d23c90
diman <zuzoev.d@ext-system.com>
2026-06-25 19:01:57 +0700

    Add OR default file checker implementation

--- src/main/java/com/es/lib/spring/service/file/FileStoreSecurityService.java
+++ src/main/java/com/es/lib/spring/service/file/FileStoreSecurityService.java
@@ -2,15 +2,15 @@ package com.es.lib.spring.service.file;
 
 import com.es.lib.entity.iface.file.IFileStore;
 
-import java.util.Collection;
-import java.util.stream.Collectors;
-
 public interface FileStoreSecurityService {
 
     boolean isFileAvailable(IFileStore fileStore);
 
-    static boolean isAvailable(IFileStore fileStore, String checker, Collection<FileStoreSecurityCheckService> checkServices) {
-        for (FileStoreSecurityCheckService service : checkServices.stream().filter(v -> v.isAccept(checker)).collect(Collectors.toList())) {
+    static boolean isAvailable(IFileStore fileStore, String checker, Iterable<FileStoreSecurityCheckService> checkServices) {
+        for (FileStoreSecurityCheckService service : checkServices) {
+            if (!service.isAccept(checker)) {
+                continue;
+            }
             if (!service.isAvailable(fileStore)) {
                 return false;
             }
